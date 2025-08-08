52e52064f18ccfc5c242c7a2162afb246323bca5
Mrityunjay Chauhan <msdchauhan1@gmail.com>
2025-08-08 12:08:29 +0530

    Update ClassA.java
    this is Created by me

--- src/com/mjay/ClassA.java
+++ src/com/mjay/ClassA.java
@@ -8,6 +8,10 @@ int add(int a,int b) {
 	System.out.println("this is your add: "+(a+b));
 	return a+b;
 }
+	public void (){
+String name="Mrityunjay Chauhan";
+		System.out.println(name);
+		System.out.println(101+100+99);
 public static void main(String[] args) {
 	ClassA C1=new ClassA();
 	C1.add(10, 90);
